a4dd18eda3bf8e390401c53d461fdb6d92a35a51
Peterson <44592787+petscaramussi@users.noreply.github.com>
2021-06-02 23:47:51 -0300

    final

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,123 +10,143 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   List _listaTarefas = [];
-  Map<String, dynamic> _ultimaTarefaRemovido = Map();
+  Map<String, dynamic> _ultimaTarefaRemovida = Map();
   TextEditingController _controllerTarefa = TextEditingController();
 
   Future<File> _getFile() async {
+
     final diretorio = await getApplicationDocumentsDirectory();
-    return File("$diretorio/dados.json");
+    return File( "${diretorio.path}/dados.json" );
+
   }
 
-  _salvarTarefa() {
-    String textoDigiitado = _controllerTarefa.text;
+  _salvarTarefa(){
+
+    String textoDigitado = _controllerTarefa.text;
 
     Map<String, dynamic> tarefa = Map();
-    tarefa["titulo"] = textoDigiitado;
+    tarefa["titulo"] = textoDigitado;
     tarefa["realizada"] = false;
+
     setState(() {
-      _listaTarefas.add(tarefa);
+      _listaTarefas.add( tarefa );
     });
     _salvarArquivo();
+
     _controllerTarefa.text = "";
+
   }
 
   _salvarArquivo() async {
+
     var arquivo = await _getFile();
 
-    String dados = json.encode(_listaTarefas);
-    arquivo.writeAsString(dados);
+    String dados = json.encode( _listaTarefas );
+    arquivo.writeAsString( dados );
+
   }
 
   _lerArquivo() async {
-    try {
+
+    try{
+
       final arquivo = await _getFile();
       return arquivo.readAsString();
-    } catch (e) {
+
+    }catch(e){
       return null;
     }
+
   }
 
   @override
   void initState() {
     super.initState();
 
-    _lerArquivo().then((dados) {
+    _lerArquivo().then( (dados){
       setState(() {
         _listaTarefas = json.decode(dados);
       });
-    });
+    } );
+
   }
 
-  Widget criarItemLista(context, index) {
+  Widget criarItemLista(context, index){
 
     //final item = _listaTarefas[index]["titulo"];
 
     return Dismissible(
-      key: Key( DateTime.now().microsecondsSinceEpoch.toString() ),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction){
+        key: Key( DateTime.now().millisecondsSinceEpoch.toString() ),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction){
 
-        //recuperar último item excluido
-        _ultimaTarefaRemovido = _listaTarefas[index];
+          //recuperar último item excluído
+          _ultimaTarefaRemovida = _listaTarefas[index];
 
+          //Remove item da lista
+          _listaTarefas.removeAt(index);
+          _salvarArquivo();
 
+          //snackbar
+          final snackbar = SnackBar(
+            //backgroundColor: Colors.green,
+            duration: Duration(seconds: 5),
+              content: Text("Tarefa removida!!"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: (){
 
-        //remove item da lista
-        _listaTarefas.removeAt(index);
-        _salvarArquivo();
+                  //Insere novamente item removido na lista
+                  setState(() {
+                    _listaTarefas.insert(index, _ultimaTarefaRemovida);
+                  });
+                  _salvarArquivo();
 
-        //snackbar
-        final snackBar = SnackBar(
-          //backgroundColor: Colors.green,
-          duration: Duration(seconds: 2),
-          content: Text("Tarefa removida!"),
-          action: SnackBarAction(
-            label: "Desfazer",
-            onPressed: (){
-              //Insere novamente item removido na lista
-              setState(() {
-                _listaTarefas.insert(index, _ultimaTarefaRemovido);                    
-              });
-              _salvarArquivo();
-              
-            },
+                }
+            ),
+          );
+
+          Scaffold.of(context).showSnackBar(snackbar);
+
+        },
+        background: Container(
+          color: Colors.red,
+          padding: EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Icon(
+                Icons.delete,
+                color: Colors.white,
+              )
+            ],
           ),
-        );
-
-        Scaffold.of(context).showSnackBar(snackBar);
-      },
-      background: Container(
-        color: Colors.red,
-        padding: EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Icon(
-              Icons.delete,
-              color: Colors.white,
-            )
-          ],
         ),
-      ),
-      child: CheckboxListTile(
-      title: Text(_listaTarefas[index]['titulo']),
-      value: _listaTarefas[index]['realizada'],
-      onChanged: (valorAlterado) {
-        setState(() {
-          _listaTarefas[index]['realizada'] = valorAlterado;
-        });
-        _salvarArquivo();
-      },
-      ),
+        child: CheckboxListTile(
+          title: Text( _listaTarefas[index]['titulo'] ),
+          value: _listaTarefas[index]['realizada'],
+          onChanged: (valorAlterado){
+
+            setState(() {
+              _listaTarefas[index]['realizada'] = valorAlterado;
+            });
+
+            _salvarArquivo();
+
+          },
+        )
     );
+
   }
+
 
   @override
   Widget build(BuildContext context) {
-    // _salvarArquivo();
-    //print("itens: " + _listaTarefas.toString() );
+
+    //_salvarArquivo();
+    //print("itens: " + DateTime.now().millisecondsSinceEpoch.toString() );
 
     return Scaffold(
       appBar: AppBar(
@@ -135,45 +155,59 @@ class _HomeState extends State<Home> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.purple,
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: Text("Adicionar Tarefa"),
-                  content: TextField(
-                    controller: _controllerTarefa,
-                    decoration: InputDecoration(labelText: "Digite sua tarefa"),
-                    onChanged: (text) {},
-                  ),
-                  actions: <Widget>[
-                    TextButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.purple,
+          onPressed: (){
+
+            showDialog(
+                context: context,
+              builder: (context){
+
+                  return AlertDialog(
+                    title: Text("Adicionar Tarefa"),
+                    content: TextField(
+                      controller: _controllerTarefa,
+                      decoration: InputDecoration(
+                        labelText: "Digite sua tarefa"
+                      ),
+                      onChanged: (text){
+
+                      },
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
                         child: Text("Cancelar"),
-                        onPressed: () => Navigator.pop(context)),
-                    TextButton(
+                        onPressed: () => Navigator.pop(context) ,
+                      ),
+                      FlatButton(
                         child: Text("Salvar"),
-                        onPressed: () {
+                        onPressed: (){
                           //salvar
                           _salvarTarefa();
                           Navigator.pop(context);
-                        }),
-                  ],
-                );
-              });
-        },
+                        },
+                      )
+                    ],
+                  );
+
+              }
+            );
+
+          }
       ),
       body: Column(
         children: <Widget>[
           Expanded(
-              child: ListView.builder(
-                  itemCount: _listaTarefas.length,
-                  itemBuilder: criarItemLista,
-                    ),
-                  )
+            child: ListView.builder(
+              itemCount: _listaTarefas.length,
+                itemBuilder: criarItemLista
+            ),
+          )
         ],
       ),
     );
   }
+
+
+
 }
